d590f27515ed5c192152b67f609c76160640f3fd
lizardsticks <lizardsticks@tamu.edu>
2018-09-25 12:04:22 -0500

    update orgonaut66 keymap

---
+++ keyboards/orgonaut66/orgonaut66.c
@@ -0,0 +1,1 @@
+#include "orgonaut66.h"
--- keyboards/planck/keymaps/lizard/keymap.c
+++ keyboards/planck/keymaps/lizard/keymap.c
@@ -28,6 +28,7 @@ enum planck_layers {
   _DVORAK,
   _LOWER,
   _RAISE,
+  _FUNCTION,
   _PLOVER,
   _ADJUST
 };
@@ -39,11 +40,14 @@ enum planck_keycodes {
   PLOVER,
   LOWER,
   RAISE,
+  FUNCTION,
   BACKLIT,
   BREATH,
   BREATH_PLUS,
   BREATH_MINUS,
-  EXT_PLV
+  EXT_PLV,
+  MACRO_1,
+  MACRO_2
 };
 
 enum keyboard_macros {
@@ -67,10 +71,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT) },
-  {KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT,LOWER,   KC_SPC,  KC_SPC,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
+  {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT) },
+  {FUNCTION, KC_LCTL, KC_LGUI, KC_LALT,LOWER,   KC_SPC,  KC_SPC,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Colemak
@@ -170,6 +174,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_PGDN, KC_PGUP, KC_MPLY}
 },
 
+
+/* Function
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   4  |   5  |   6  |   ]  |  \   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   1  |   2  |   3  |   [  |   ]  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      | Next |Pg Dn |Pg Up | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_FUNCTION] = {
+  {KC_TILD,  MACRO_1, MACRO_2, KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
+  {KC_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,    KC_6,    KC_RBRC, KC_PIPE},
+  {KC_LSFT,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,    KC_2,    KC_3,    KC_LBRC, KC_RBRC},
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_PGDN, KC_PGUP, KC_MPLY}
+},
+
+
 /* Plover layer (http://opensteno.org)
  * ,-----------------------------------------------------------------------------------.
  * |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |
@@ -201,7 +225,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL },
+  {_______, RESET,   DEBUG,   MACRO_1, MACRO_2, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL },
   {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  PLOVER,  _______},
   {KC_CAPS, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  BREATH, BREATH_PLUS, BREATH_MINUS, _______, _______},
   {BACKLIT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
@@ -217,8 +241,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   float my_song[][2] = SONG(IMPERIAL_MARCH);
 #endif
 
+void update_quad_layer(uint8_t layer1, uint8_t layer2, uint8_t layer3, uint8_t layer4) {
+  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2) && IS_LAYER_ON(layer3)) {
+    layer_on(layer4);
+  } else {
+    layer_off(layer4);
+  }
+}
+
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+	  
+	  
+	case MACRO_1:
+	  if (record->event.pressed) {
+		SEND_STRING("cd /mnt/c/Users/EJ/Documents");
+		register_code(KC_ENT);
+		return false; 
+	  }
+	case MACRO_2:
+	  if (record->event.pressed) {
+		  //navigate to folder
+		SEND_STRING("cd /mnt/c/Users/EJ/Documents/Github/qmk_lizard");
+		register_code(KC_ENT);
+		unregister_code(KC_ENT);
+		
+		//send command to compile
+		SEND_STRING("make planck:lizard");
+		register_code(KC_ENT);
+		unregister_code(KC_ENT);
+		
+		return false; 
+	  }
+	
+	  
+	  
     case QWERTY:
       if (record->event.pressed) {
         print("mode just switched to qwerty and this is a huge string\n");
@@ -247,9 +305,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           //breathing_period_set(2);
           //breathing_pulse();
         #endif
+        //update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
+        //update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
@@ -259,13 +319,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_RAISE);
         //breathing_period_set(2);
         //breathing_pulse();
+		//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
+        //update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
+	  
+	case FUNCTION:
+      if (record->event.pressed) {
+        layer_on(_FUNCTION);
+        //breathing_period_set(2);
+        //breathing_pulse();
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+		//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_FUNCTION);
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+		//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+        //update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+	  
     case BACKLIT:
       if (record->event.pressed) {
         register_code(KC_RSFT);
