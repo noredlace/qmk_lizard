b29a107346e8612ce9316d487b87e7b58ba39467
lizardsticks <lizardsticks@tamu.edu>
2019-03-26 16:13:04 -0500

    update keycrawl pad for voice meeter mixer

--- keyboards/2key2crawl/keymaps/lizard/keymap.c
+++ keyboards/2key2crawl/keymaps/lizard/keymap.c
@@ -3,6 +3,8 @@
 
 enum crawl_layers {
   _QWERTY,
+  _VOICEMEETERA1,
+  _VOICEMEETERA2,
   _LOWER,
   _RAISE,
   _FUNCTION,
@@ -11,6 +13,8 @@ enum crawl_layers {
 
 enum crawl_keycodes {
   QWERTY = SAFE_RANGE,
+  VOICEMEETERA1,
+  VOICEMEETERA2,
   LOWER,
   RAISE,
   TEST,
@@ -23,20 +27,29 @@ enum crawl_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_QWERTY] = KEYMAP(
-    KC_1, KC_2, KC_3, KC_4, RESET,
-    KC_Q, LOWER, KC_SPACE, RAISE, KC_0, KC_ENT),
+    VOICEMEETERA1, VOICEMEETERA2, KC_F13, KC_F14, RAISE,
+    KC_F15, KC_F16, KC_F17, KC_F18, LOWER, KC_ENT),
+
+//state for voice meeter volume adjust
+   	[_VOICEMEETERA1] = KEYMAP(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT),
+
+    [_VOICEMEETERA2] = KEYMAP(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT),
 
 	[_LOWER] = KEYMAP(
-    KC_1, KC_2, KC_3, KC_4, RESET,
-    KC_L, KC_TRNS, KC_0, RAISE, KC_0, KC_ENT),
+    KC_1, KC_2, KC_3, KC_4, KC_TRNS,
+    KC_L, KC_TRNS, KC_0, KC_TRNS, KC_TRNS, KC_ENT),
 
 	[_RAISE] = KEYMAP(
-    KC_1, KC_2, KC_3, KC_4, RESET,
-    KC_R, LOWER, KC_BSPC, KC_TRNS, KC_0, KC_ENT),
+    KC_1, KC_2, KC_3, KC_4, KC_TRNS,
+    KC_R, LOWER, KC_BSPC, KC_TRNS, KC_TRNS, KC_ENT),
 
 	[_ADJUST] = KEYMAP(
-    KC_1, KC_2, KC_3, KC_4, RESET,
-    KC_A, KC_TRNS, KC_BSPC, KC_TRNS, KC_0, RESET)
+    KC_1, KC_2, KC_3, KC_4, KC_TRNS,
+    KC_A, KC_TRNS, KC_BSPC, KC_TRNS, KC_TRNS, RESET)
 	
 };
 
@@ -73,21 +86,77 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			return false;
 			break;
 
+		case VOICEMEETERA1:
+			if (record->event.pressed) {
+				layer_on(_VOICEMEETERA1);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_VOICEMEETERA1);
+				//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+
+		case VOICEMEETERA2:
+			if (record->event.pressed) {
+				layer_on(_VOICEMEETERA2);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			} else {
+				layer_off(_VOICEMEETERA2);
+				//update_quad_layer(_LOWER, _RAISE, _FUNCTION, _ADJUST);
+				//update_tri_layer(_LOWER, _RAISE, _ADJUST);
+			}
+			return false;
+			break;
+
+
 
 	}
 	return true;
 }
 
 void encoder_update_user(int8_t index, bool clockwise) {
-  if (index == 0) {
-    if (clockwise) {
-      register_code(KC_PGUP);
-      unregister_code(KC_PGUP);
-    } else {
-      register_code(KC_PGDOWN);
-      unregister_code(KC_PGDOWN);
-    }
-  }
+
+
+	if (IS_LAYER_ON(_QWERTY)){
+		if (index == 0) {
+			if (clockwise) {
+			register_code(KC_PGUP);
+			unregister_code(KC_PGUP);
+			} 
+			else {
+			register_code(KC_PGDN);
+			unregister_code(KC_PGDN);
+			}
+		}
+	}
+
+	//adjust voice meeter output volume
+	if (IS_LAYER_ON(_VOICEMEETERA1)){
+		if (index == 0) {
+			if (clockwise) {
+			register_code(KC_F21);
+			unregister_code(KC_F21);
+		} 
+		else {
+			register_code(KC_F22);
+			unregister_code(KC_F22);
+		}
+		}
+	}
+	if (IS_LAYER_ON(_VOICEMEETERA2)){
+		if (index == 0) {
+			if (clockwise) {
+			register_code(KC_F23);
+			unregister_code(KC_F23);
+		} 
+		else {
+			register_code(KC_F24);
+			unregister_code(KC_F24);
+		}
+		}
+	}
 }
 
 
